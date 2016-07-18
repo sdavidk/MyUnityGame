40d93a1b75d969519e4f879248053810945b29db
sdavidk <davidkohno500@gmail.com>
2016-07-18 09:23:35 -0400

    update

---
+++ MyUnityGame/Assets/AEgis_Technologies/Script/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+   public Rigidbody rb;
+    public float speed;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); //Get component of rigidbody.
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical); //Constructor to create vector3
+
+        rb.AddForce(movement * 10);
+    }
+    
+}
